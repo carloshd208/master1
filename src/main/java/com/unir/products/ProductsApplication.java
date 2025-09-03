c41805f06ec737e239bc30bbbde5b75bd6469e25
juan <juan@hotmail.com>
2025-09-03 12:37:44 -0600

    quinta version

--- src/main/java/com/unir/products/ProductsApplication.java
+++ src/main/java/com/unir/products/ProductsApplication.java
@@ -1,7 +1,9 @@
 package com.unir.products;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductsApplication {
@@ -9,5 +11,15 @@ public class ProductsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProductsApplication.class, args);
 	}
+        
+        @Bean
+    public CommandLineRunner checkEnv() {
+        return args -> {
+            System.out.println("MYSQLHOST: " + System.getenv("MYSQLHOST"));
+            System.out.println("MYSQLUSER: " + System.getenv("MYSQLUSER"));
+            System.out.println("MYSQLDATABASE: " + System.getenv("MYSQLDATABASE"));
+            System.out.println("MYSQLPORT: " + System.getenv("MYSQLPORT"));
+        };
+    }
 
 }
